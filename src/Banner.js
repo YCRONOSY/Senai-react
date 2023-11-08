4f7fc4ae93e0889c1d226a45ccbd4c600d599263
Cronos <alunosenai@gmail.com>
2023-11-08 05:15:10 -0300

    test

---
+++ src/Banner.js
@@ -0,0 +1,10 @@
+//import './Banner.css'
+
+export const Banner = ()=>{
+    //35x
+    return(
+        <header className='banner'>
+            <img src="/imagem/SENAI.pmg" alt="o banner principal da pagina do "></img>
+        </header>
+    )
+}
